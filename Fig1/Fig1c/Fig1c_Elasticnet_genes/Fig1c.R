89a7c6b80f71c4421c4207374ad14d07af0fe1d0
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-07-03 13:46:59 +0530

    Updated codes

---
+++ Fig1/Fig1c/Fig1c_Elasticnet_genes/Fig1c.R
@@ -0,0 +1,50 @@
+##loading libraries
+library(caret)
+library(glmnet)
+
+##loading models
+x=load("Model_enet_1.RData")
+model1= get(x)
+x=load("Model_enet_2.RData")
+model2= get(x)
+
+x=load("Model_enet_3.RData")
+model3= get(x)
+
+x=load("Model_enet_4.RData")
+model4= get(x)
+
+x=load("Model_enet_5.RData")
+model5= get(x)
+
+
+load("Test_set.Rdata")
+drugs = unique(Test_set$drug_name)
+
+
+correlation = list()
+for (i in unique(drugs)){
+  
+  
+  pos = which(Test_set$drug_name==i)
+  test1 = Test_set[pos,]
+  
+  xtest = as.matrix(test1[,3:602])
+  IC50 = test1[,603]
+
+
+
+##Predictions
+prediction1 = predict(model1, xtest)
+prediction2 = predict(model2, xtest)
+prediction3 = predict(model3, xtest)
+prediction4 = predict(model4, xtest)
+prediction5 = predict(model5, xtest)
+
+predictions = apply(cbind.data.frame(prediction1,prediction2,prediction3,prediction4,prediction5),1,mean)
+correlation[[i]] = cor(predictions, IC50)
+
+}
+
+cor = do.call(rbind,correlation)
+
