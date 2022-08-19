5ca34961bd17e8cc25042c132acb5e224e263944
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-08-19 10:50:18 +0530

    Update FigS5b.R

--- Supplementary/S5/FigS5b/FigS5b.R
+++ Supplementary/S5/FigS5b/FigS5b.R
@@ -9,8 +9,7 @@ predictions = read.csv("ATTX_predictions.csv",sep=",",header = T,stringsAsFactor
 metadata = read.csv("Metadata.csv",sep=",",header = T,stringsAsFactors = F)
 colnames(metadata)[1] = "variable"
 
-
-##Converting IC50 to z-scores
+##Converting LN IC50 to z-scores
 sdmean = read.csv("Drugs_means_sd.csv",sep=",",header = T,stringsAsFactors = F,row.names = 1)
 pred = merge(sdmean,predictions,by=0)
  Predictions = (pred[,5:ncol(pred)] - pred[,3])/pred[,4]
@@ -25,17 +24,3 @@ df = merge(pred,metadata,by="variable")
 df$Samples <- factor(df$Samples, levels = c("PRE-CX", "POST-CX","CRPC","ENZS" ,"ENZR"))
 ggplot(df, aes(x=Samples, y=value,col=Samples)) + 
   geom_boxplot(outlier.shape = NA)+scale_color_manual( values=c(CRPC="#E41A1C",ENZR="#377EB8",ENZS="#4DAF4A","PRE-CX"="#984EA3","POST-CX"="#FF7F00"))+theme_classic(base_size = 20)+theme(axis.text.x = element_text(angle = 45, hjust=1))+ylab("Predicted  LN IC50 (Z-score)")
-
-
-##Wilcoxon test
-PRECX  = df[which(df$Samples=="PRE-CX"),]
-POSTCX  = df[which(df$Samples=="POST-CX"),]
-CRPC  = df[which(df$Samples=="CRPC"),]
-ENZS  = df[which(df$Samples=="ENZS"),]
-ENZR  = df[which(df$Samples=="ENZR"),]
-
-wilcox.test(PRECX$value,POSTCX$value)
-wilcox.test(PRECX$value,CRPC$value)
-wilcox.test(PRECX$value,ENZS$value)
-wilcox.test(PRECX$value,ENZR$value)
-wilcox.test(ENZS$value,ENZR$value)
