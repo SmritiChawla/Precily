6334527edb97e4370f50c68c7ecc4c0027fbd6a5
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-08-19 10:30:11 +0530

    Update Fig1c.R

--- Fig1/Fig1c/Fig1c_Precily_genes/Fig1c.R
+++ Fig1/Fig1c/Fig1c_Precily_genes/Fig1c.R
@@ -1,26 +1,16 @@
 ##Loading libraries
 library(keras)
-library(tidyverse)
-library(pheatmap)
-library(data.table)
-library(caret)
 
-
+##load test dataset
 test= read.csv("Test_data.csv",sep=",",header=T,stringsAsFactors = F)
-
 drugs = unique(test$X1)
 
 correlation = list()
-
 for (i in unique(drugs)){
-  
-  
   pos = which(test$X1==i)
   test1 = test[pos,]
-  
   testd = as.matrix(test1[,3:602])
   IC50 = test1[,603]
-  
   
   ##loading models
   model1 = load_model_hdf5("Model1.hdf5")
@@ -29,15 +19,12 @@ for (i in unique(drugs)){
   model4 = load_model_hdf5("Model4.hdf5")
   model5 = load_model_hdf5("Model5.hdf5")
   
-  
-  
   ##Making predictions
   prediction1=model1 %>% predict(testd)
   prediction2=model2 %>% predict(testd)
   prediction3=model3 %>% predict(testd)
   prediction4=model4 %>% predict(testd)
   prediction5=model5 %>% predict(testd)
-  
   
   ##Averaging out predictions
   predictions = apply(cbind.data.frame(prediction1,prediction2,prediction3,prediction4,prediction5),1,mean)
@@ -46,4 +33,5 @@ for (i in unique(drugs)){
   
 }
 
+##Correlation
 cor = do.call(rbind,correlation)
