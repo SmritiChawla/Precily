3b7a631afc5a8aba30ef7fea7f23d52376a27b88
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-08-23 10:53:45 +0530

    Update Fig3c.R

--- Fig3/Fig3c/Fig3c.R
+++ Fig3/Fig3c/Fig3c.R
@@ -4,7 +4,6 @@ library(impute)
 library(tidyverse)
 library(ggpubr)
 
-
 ##Source function
 source("DrugsPred.R")
 
@@ -17,10 +16,10 @@ load("GDSC2_metadata.RData")
 ##Drug response prediction
 df1 = drugPred(enrichment.scores,metadata,"PRAD")
 
-
 ##Reduce list to dataframe
 Pred=df1 %>% purrr::reduce(left_join, by = "DRUGS")
 
+##Extract original labels of LNCaP cell line
 pos = which(metadata$CELL_LINE_NAME=="LNCAPCLONEFGC")
 meta = metadata[pos,]
 
@@ -28,12 +27,10 @@ meta = metadata[pos,]
 LNCAP_gdsc = cbind.data.frame(meta$DRUG_NAME,meta$LN_IC50)
 colnames(LNCAP_gdsc)[1] = "DRUGS"
 
-
 ##LNCAP1
 LNCAP1 = Pred[,c(1,4)]
 mat1 = merge(LNCAP_gdsc,LNCAP1,by="DRUGS")
 colnames(mat1) = c("DRUGS","Real","Predicted")
-
 
 ##LNCAP2
 mat2 = Pred[,c(1,5)]
